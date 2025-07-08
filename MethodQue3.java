9fb04b2a637c841fdf11f18131ea793ac6885a45
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-07-08 14:43:39 +0530

    hui

---
+++ MethodQue3.java
@@ -0,0 +1,27 @@
+//A person is eligible to vote if his/her age is greater than or equal to 18. Define a method to find out if he/she is eligible to vote.
+
+import java.util.Scanner;
+
+public class MethodQue3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner( System.in);
+        System.out.println("Enter person's age : ");
+        int age = sc.nextInt();
+
+        if(eligibleToVote(age)){
+            System.out.println("Person is eligible to vote");
+        }else{
+            System.out.println("Person is not eligible to vote");
+
+        }
+    }
+    static boolean eligibleToVote(int age){
+        if(age >=18) {
+            return true;
+        }
+        return false;
+
+    }
+    
+    
+}
