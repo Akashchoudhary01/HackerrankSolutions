f2c3f6d581867323dbc3054f4ad321f144a8f65c
Akash Choudhary <akashkrchoudhary007@gmail.com>
2025-07-10 13:36:29 +0530

    Maximum of array

---
+++ MaxArray.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the element of your Arrays");
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for(int n : arr){
+            System.out.print("Element in array : "+n);
+        }
+        System.out.println("Maxium num is : "+MaxOf(arr));
+        
+
+
+        
+
+    }
+
+    static int MaxOf(int[] arr) {
+        int maxValue = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+
+            }
+
+        }
+        return maxValue;
+    }
+}
